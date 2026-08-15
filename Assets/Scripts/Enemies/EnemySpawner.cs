415553e93a887eddd4efdedb888fe1e7dc0dfda3
M-Fsu <mikeehendz@gmail.com>
2026-08-15 15:14:10 -0400

    Wave spawner finished along with color spawning for enemies
    Wave spawner completed able to spawn waves stop spawning spawn different types and different colors
    
    Enemies correctly get assigned model per enemy type and the correct color model is enabled afterwards

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,155 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class EnemySpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public int totalEnemiesInWave = 10;
+        public float timeToSpawn = 1f;
+
+        [Range(0, 100)] public float simpleRatio;
+        [Range(0, 100)] public float chargerRatio;
+        [Range(0, 100)] public float shooterRatio;
+    }
+    public GameObject enemy;
+    public Transform playerPos;
+    
+    public List<Wave> waves;
+    public float timeBetweenWaves = 5f;
+
+    public Vector3 mapCenter = Vector3.zero;
+    public float spawnRadius = 20f;
+    public float minDistanceToPlayer = 5f;
+
+    private int waveIndex = 0;
+    private bool isSpawning = false;
+    private List<GameObject> activeEnemies = new List<GameObject>();
+    
+
+    private void Start()
+    {
+        if (playerPos == null)
+        {
+            playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        if (waves.Count > 0)
+        {
+            StartCoroutine(WaveLoop());
+        }
+    }
+
+    IEnumerator WaveLoop()
+    {
+        while (waveIndex < waves.Count)
+        {
+            isSpawning = true;
+            yield return StartCoroutine(SpawnWave(waves[waveIndex]));
+            isSpawning = false;
+
+            while (activeEnemies.Count > 0)
+            {
+                activeEnemies.RemoveAll(item => item == null);
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            yield return new WaitForSeconds(timeBetweenWaves);
+
+            waveIndex++;
+        }
+    }
+    EnemyBehavior.EnemyType GetTypeFromRatio(Wave wave)
+    {
+        float totalWeight = wave.simpleRatio + wave.shooterRatio + wave.chargerRatio;
+        if (totalWeight <= 0) return EnemyBehavior.EnemyType.Simple;
+        float roll = Random.Range(0f, totalWeight);
+
+        if (roll < wave.simpleRatio)
+        {
+            return EnemyBehavior.EnemyType.Simple;
+        }
+        else if (roll < wave.shooterRatio + wave.simpleRatio)
+        {
+            return EnemyBehavior.EnemyType.Shooter;
+        }
+        else
+        {
+            return EnemyBehavior.EnemyType.Charger;
+        }
+
+    }
+    IEnumerator SpawnWave(Wave wave)
+    {
+        for (int i = 0; i < wave.totalEnemiesInWave; i++)
+        {
+            EnemyBehavior.EnemyType randType = GetTypeFromRatio(wave);
+
+            Vector3 validSpawn = GetValidSpawn();
+            SpawnEnemy(randType, validSpawn);
+
+            yield return new WaitForSeconds(wave.timeToSpawn);
+
+        }
+    }
+
+    Vector3 GetValidSpawn()
+    {
+        Vector3 spawnPos = Vector3.zero;
+        bool valid = false;
+
+        while (!valid)
+        {
+            Vector2 randomCirclePoint = Random.insideUnitCircle * spawnRadius;
+            spawnPos = new Vector3(randomCirclePoint.x, 0f, randomCirclePoint.y) + mapCenter;
+            if (playerPos != null)
+            {
+                float distanceToPlayer = Vector3.Distance(spawnPos, playerPos.position);
+                if (distanceToPlayer >= minDistanceToPlayer)
+                {
+                    valid = true;
+                }
+            }
+            else 
+            { 
+                valid = true; 
+            }
+        }
+        return spawnPos;
+    }
+
+    void SpawnEnemy(EnemyBehavior.EnemyType TypeToSpawn, Vector3 spawnPos)
+    {
+        GameObject newEnemy = Instantiate(enemy, spawnPos, Quaternion.identity);
+        activeEnemies.Add(newEnemy);
+
+        EnemyStats enemyStats = newEnemy.GetComponent<EnemyStats>();
+        EnemyBehavior enemyBehavior = newEnemy.GetComponent<EnemyBehavior>();
+
+        enemyStats.type = TypeToSpawn;
+        enemyBehavior.player = playerPos;
+
+        System.Array colors = System.Enum.GetValues(typeof(gameManager.ColorType));
+        gameManager.ColorType randColor = (gameManager.ColorType)colors.GetValue(Random.Range(0, colors.Length - 1));
+        enemyStats.Color = randColor;
+
+        if (TypeToSpawn == EnemyBehavior.EnemyType.Simple)
+        {
+            enemyStats.speed = 3f;
+            enemyStats.maxHp = 100f;
+        }
+        else if(TypeToSpawn == EnemyBehavior.EnemyType.Charger)
+        {
+            enemyStats.speed = 5f;
+            enemyStats.maxHp = 150f;
+        }
+        else if(TypeToSpawn == EnemyBehavior.EnemyType.Shooter)
+        {
+            enemyStats.speed = 2f;
+            enemyStats.maxHp = 70f;
+        }
+        enemyStats.LoadModel();
+    }
+
+}
--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -7,15 +7,74 @@ public class EnemyStats : MonoBehaviour
     public EnemyBehavior.EnemyType type;
     public gameManager.ColorType Color;
     // Update is called once per frame
-    
+
     public float maxHp = 100f;
     public float currentHp;
     public float speed;
     public float damage;
+
+
+    public GameObject simpleModel;
+    public GameObject chargerModel;
+    public GameObject shooterModel;
+
+
     void Awake()
     {
         currentHp = maxHp;
     }
 
-    
+    public void LoadModel()
+    {
+        GameObject model = null;
+
+        switch (type)
+        {
+            case EnemyBehavior.EnemyType.Simple:
+                model = simpleModel;
+                break;
+            case EnemyBehavior.EnemyType.Charger:
+                model = chargerModel;
+                break;
+            case EnemyBehavior.EnemyType.Shooter:
+                model = shooterModel;
+                break;
+
+        }
+
+        if (model != null)
+        {
+            GameObject spawndedModel = Instantiate(model, transform.position, transform.rotation);
+            spawndedModel.transform.SetParent(this.transform);
+
+            SetModelColor(spawndedModel);
+        }
+    }
+    private void SetModelColor(GameObject model)
+    {
+        string ColorName = Color.ToString(); ;
+
+
+        foreach (Transform child in model.GetComponentsInChildren<Transform>(true))
+        {
+            if (child == model.transform) continue;
+            if (child.name.Equals(ColorName, System.StringComparison.OrdinalIgnoreCase))
+            {
+                child.gameObject.SetActive(true);
+
+            }
+            else
+            {
+                if (IsAnEnemyColorName(child.name))
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+    private bool IsAnEnemyColorName(string name)
+    {
+        string upperName = name.ToUpper();
+        return upperName == "RED" || upperName == "ORANGE" || upperName == "YELLOW" || upperName == "GREEN" || upperName == "BLUE" || upperName == "PURPLE";
+            }
 }
